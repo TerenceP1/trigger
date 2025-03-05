af09b58d36645c8901c0861460cc759c31a8e159
TerenceP1 <158529801+TerenceP1@users.noreply.github.com>
2025-03-05 16:40:13 -0500

    Update cmd.py

--- cmd.py
+++ cmd.py
@@ -8,10 +8,42 @@ if len(sys.argv)==1:
 token="7916790982:AAHfrFvAHSYtCw5Qx6DhmdAGcq2yE6H5yYo"
 if sys.argv[1]=="0":
     offset=0
-    tstart=
+    tstart=(datetime.utcnow() - datetime(1970, 1, 1)).total_seconds()
     while True:
-        print(
-            requests.post(
-                f"https://api.telegram.org/bot{token}/getUpdates",
-                data={
-                    
+        rq=requests.post(
+            f"https://api.telegram.org/bot{token}/getUpdates",
+            data={
+                "offset": offset,
+                "allowed_updates": ["message"]
+            }
+        )
+        #print(rq.text)
+        rqj=json.loads(rq.text)
+        for i in rqj:
+            offset=int(i["update_id"])
+            if i["message"]["from"]["username"]!="terryp12":
+                #print("PHONY")
+                continue
+            if i["message"]["date"]<tstart:
+                #print("EARLY")
+                continue
+            print(i["message"]["text"])
+
+rq=requests.post(
+    f"https://api.telegram.org/bot{token}/getUpdates",
+    data={
+        "offset": offset,
+        "allowed_updates": ["message"]
+    }
+)
+js=json.loads(rq)
+cid=int(js[0]["message"]["chat"]["id"])
+while True:
+    prm=input()
+    requests.post(
+        f"https://api.telegram.org/bot{token}/sendMessage",
+        data={
+            "chat_id": cid,
+            "text": prm
+        }
+    )
