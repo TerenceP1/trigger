a8acb4f5c7ca7465207a5fce12dfe4d8e8084941
TerenceP1 <158529801+TerenceP1@users.noreply.github.com>
2025-03-10 15:07:43 -0400

    Update download.py

--- download.py
+++ download.py
@@ -1,8 +1,8 @@
 import httpx
 a=open("gogogo.bat",'x')
 with a as b:
-    b.write(bytes(httpx.get("https://raw.githubusercontent.com/TerenceP1/trigger/refs/heads/main/gogogo.bat").text,"utf-8"))
+    b.write(httpx.get("https://raw.githubusercontent.com/TerenceP1/trigger/refs/heads/main/gogogo.bat").text)
 
 a=open("cmd.py",'x')
 with a as b:
-    b.write(bytes(httpx.get("https://raw.githubusercontent.com/TerenceP1/trigger/refs/heads/main/cmd.py").text,"utf-8"))
+    b.write(httpx.get("https://raw.githubusercontent.com/TerenceP1/trigger/refs/heads/main/cmd.py").text)
